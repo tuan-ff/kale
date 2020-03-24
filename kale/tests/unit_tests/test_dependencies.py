tuan-ff/kale

commit eac342612358f67a5b04d8d7869aa0a7b391d8b6

backend: New dependencies detection algorithm (#81)
* Remove unused constant

Signed-off-by: Stefano Fioravanzo <stefano@arrikto.com>

* Add graph function to compute ancestors

The function provided by Netowrkx to compute ancestors (nx.ancestors)
does not produce an ordered results with respect to DAG layers. Kale
needs to traverse the graph in reverse topological order, preserving the
ordering of the DAG Layers.

Signed-off-by: Stefano Fioravanzo <stefano@arrikto.com>

* Add helpers to parse functions from source code

These function will be used in the next commit (new dependencies
detection algorithm) in order to parse function present in the source
code using the ast library. These function perform operations like
getting all the function calls in a code block, getting all the args of
a function or its body.

Signed-off-by: Stefano Fioravanzo <stefano@arrikto.com>

* Add function to detect free variables

This function gets all the free variables of all the global functions
present in an input source code.

Signed-off-by: Stefano Fioravanzo <stefano@arrikto.com>

* Implement a new dependencies detection algorithm

For implementation details about the new algortihm, refer to the
docstring of the function `static_analysis/dependencies_detection`.

Signed-off-by: Stefano Fioravanzo <stefano@arrikto.com>

* Update tests

Signed-off-by: Stefano Fioravanzo <stefano@arrikto.com>

Reviewed-by: Ilias Katsakioris <elikatsis@arrikto.com>

kale/static_analysis/ast.py
@@ -14,41 +14,32 @@
 
 import re
 import ast
+import astor
 
 from collections import deque
 from kale.utils import utils
-
-# TODO: Get this list dynamically
-_BUILT_INS_ = ["abs", "delattr", "hash", "memoryview", "set",
-               "all", "dict", "help", "min", "setattr",
-               "any", "dir", "hex", "next", "slice",
-               "ascii", "divmod", "id", "object", "sorted",
-               "bin", "enumerate", "input", "oct", "staticmethod",
-               "bool", "eval", "int", "open", "str",
-               "breakpoint", "exec", "isinstance", "ord", "sum",
-               "bytearray", "filter", "issubclass", "pow", "super",
-               "bytes", "float", "iter", "print", "tuple",
-               "callable", "format", "len", "property", "type",
-               "chr", "frozenset", "list", "range", "vars",
-               "classmethod", "getattr", "locals", "repr", "zip",
-               "compile", "globals", "map", "reversed", "__import__",
-               "complex", "hasattr", "max", "round"]
+from functools import lru_cache
 
 
-def walk(node, skip_nodes=tuple()):
+def walk(node, stop_at=tuple(), ignore=tuple()):
     """Walk through the children of an ast node.
 
     Args:
         node: an ast node
-        skip_nodes: stop traversing through these nodes
+        stop_at: stop traversing through these nodes, including the matching
+            node
+        ignore: stop traversing through these nodes, excluding the matching
+            node
 
     Returns: a generator of ast nodes
-
     """
     todo = deque([node])
     while todo:
         node = todo.popleft()
-        if not isinstance(node, skip_nodes):
+        if isinstance(node, ignore):
+            # dequeue next node
+            continue
+        if not isinstance(node, stop_at):
             next_nodes = ast.iter_child_nodes(node)
             for n in next_nodes:
                 todo.extend([n])
@@ -75,30 +66,37 @@ def get_list_tuple_names(node):
     return names
 
 
-def get_all_names(code):
-    """Get all matching nodes from the ast of the input code block.
+@lru_cache(maxsize=128)
+def get_marshal_candidates(code):
+    """Get all the names that could be selected as objects to be marshalled.
 
-    Matching nodes:
+    This function is used by a descendant node onto its ancestors to resolve
+    its missing dependencies. Example:
 
+    +---+     +---+     +---+
+    | A | --> | B | --> | C |
+    +---+     +---+     +---+
+
+    When C runs the PyFlakes report on its source code, `x` is detected as a
+    missing variable. Then C runs `get_marshal_candidates` on its ancestors,
+    in order, starting from B. All the marshal candidates found in B that match
+    any of the missing C's names, as set as B's `outs`.
+
+    Ast nodes that become candidates:
         - ast.Name
-        - ast.FunctionDef
-        - ast.ClassDef
         - ast.Import
         - ast.ImportFrom
         - ast.Tuple
 
-    This function is just used to make a cross reference with the missing names
-    detected by the Flakes report. It is not used to arbitrary detect variable
-    dependencies.
-
-    Known missing detections:
-
-        - Function and Class parameters
+    Ast nodes that create local contexts must be excluded from the search, as
+    they can crete local variables that alias global ones. These nodes include
+    ast.FunctionDef, ast.ClassDef, context manager names and list and dict
+    comprehensions variables.
 
     Args:
-        code: multiple string representing Python code
+        code (str): multiple string representing Python code
 
-    Returns: a list of string names
+    Returns (list(str)): a list of names
     """
     names = set()
 
@@ -114,14 +112,24 @@ def get_all_names(code):
     # Note #3: Magic commands are preserved in the resulting Python executable,
     #  they are commented just here in order to make AST run.
     commented_code = utils.comment_magic_commands(code)
-
+    # need to exclude all the nodes that mights *define* variables in a local
+    # scope. For example, a function may define a variable x that is aliasing
+    # a global variable x, and we don't want to marshal it in that step, but
+    # from the step that defines the global x.
+    # TODO: Search for all possible python nodes that define local vars.
+    #  List comprehensions ([i for i in list])
+    #  Dict comprehensions
+    #  Exception handling?
+    #  Decorators?
+    #  Context manager (just the alias)
+    contexts = (ast.FunctionDef, ast.ClassDef, )
     tree = ast.parse(commented_code)
     for block in tree.body:
         for node in walk(block):
+            if isinstance(node, contexts):
+                break
             if isinstance(node, (ast.Name,)):
                 names.add(node.id)
-            if isinstance(node, (ast.FunctionDef, ast.ClassDef,)):
-                names.add(node.name)
             if isinstance(node, (ast.Import, ast.ImportFrom,)):
                 for _n in node.names:
                     if _n.asname is None:
@@ -131,6 +139,93 @@ def get_all_names(code):
             if isinstance(node, (ast.Tuple, ast.List)):
                 names.update(get_list_tuple_names(node))
     return names
+
+
+def get_function_args(fn):
+    """Get argument names of an ast.FunctionDef node.
+
+    Ref: https://greentreesnakes.readthedocs.io/en/latest/nodes.html#function-and-class-definitions  # noqa: E501
+
+    Args:
+        fn: ast.FunctionDef node
+
+    Returns (list(str)): list of function arguments
+    """
+    fn_args = []
+    for attr in ["args", "posonlyargs", "kwonlyargs"]:
+        for arg in getattr(fn.args, attr, []):
+            fn_args.append(arg.arg)
+    if fn.args.vararg:
+        fn_args.append(fn.args.vararg.arg)
+    if fn.args.kwarg:
+        fn_args.append(fn.args.kwarg.arg)
+    return fn_args
+
+
+def parse_functions(code):
+    """Parse all the global functions present in the input code.
+
+    Parse all the ast nodes ast.FunctionDef that are global functions in the
+    source code. These also include function that are defined inside other
+    Python statements, like `try`. ast.ClassDef nodes are skipped from the
+    parsing so that class functions are ignored.
+    All the functions' arguments names are returned alongside the source code.
+
+    Args:
+        code (str): Multiline string representing Python code
+
+    Returns (dict): A dictionary [fn_name] -> (body, args)
+    """
+    fns = dict()
+    tree = ast.parse(code)
+    for block in tree.body:
+        for node in walk(block,
+                         stop_at=(ast.FunctionDef,),
+                         ignore=(ast.ClassDef,)):
+            if isinstance(node, (ast.FunctionDef,)):
+                fn_name = node.name
+                fn_args = get_function_args(node)
+                fn_body = "".join([astor.to_source(node)
+                                   for node in node.body])
+                fns[fn_name] = (fn_body, set(fn_args))
+    return fns
+
+
+def get_function_calls(code):
+    """Get all function names that are called in the input source code.
+
+    A function call is something like:
+
+    ```
+    foo()
+    ```
+
+    That is obviously different from:
+
+    ```
+    obj.fn()
+    ```
+
+    These are parsed from the source code as `ast.Call` nodes, where the
+    `func` attribute of the `ast` node is a `ast.Name` node.
+    This is guaranteed to be a 'simple' function call (first example).
+
+    Args:
+        code (str): Multiline string representing Python code
+
+    Returns (list(str)): List of function names
+    """
+    fns = set()
+    tree = ast.parse(code)
+    for block in tree.body:
+        for node in walk(block):
+            # a function call. We check the attribute func to be ast.Name
+            # because it could also be a ast.Attribute node, in case of
+            # function calls like obj.foo()
+            if (isinstance(node, (ast.Call,))
+                    and isinstance(node.func, (ast.Name,))):
+                fns.add(node.func.id)
+    return fns
 
 
 def get_function_and_class_names(code):
@@ -169,7 +264,7 @@ def parse_assignments_expressions(code):
                 "Must provide just primitive types assignments "
                 "in variables block")
         targets = block.targets
-        if isinstance(targets[0], (ast.Tuple, ast.List, )) or len(targets) > 1:
+        if isinstance(targets[0], (ast.Tuple, ast.List,)) or len(targets) > 1:
             raise ValueError(
                 "Must provide single variable "
                 "assignments in variables block")

commit 4ed01f0c7df05cbf6b063aa59452c623d8d750fd

backend: Improve dependencies detection algorithm (#83)
* Fix a regression introduced in #81

During the detection of all the candidates to be marshalled from a step,
functions and classes were skipped. In this way steps could not marshal
functions or classes. This commit fixes that.

Signed-off-by: Stefano Fioravanzo <stefano@arrikto.com>

* Rename variable to avoid potential conflicts

Signed-off-by: Stefano Fioravanzo <stefano@arrikto.com>

* Save node's graph info at the end of iteration

Signed-off-by: Stefano Fioravanzo <stefano@arrikto.com>

* Search for chained deps inside fns with free vars

Functions can be dependent on variables outside of the scope the
function body (free variables). These free variables can be themselves
functions that in turn use other free variables. This chain of
dependecies can be arbitrary long. This commit covers this case by
iteratively searching for the available functions in a step.

Signed-off-by: Stefano Fioravanzo <stefano@arrikto.com>

* Update tests

Signed-off-by: Stefano Fioravanzo <stefano@arrikto.com>

* Correct parsing of functions with a return stmt

By parsing the function def statement first and just then parsing the
body detecting the missing name with pyflakes, the report would not
detect missing names on the `return` line, becase the report would just
say that it was outside of a function.

This commit solves that, by simplifying how functions are parsed, since
having individual arguments was not needed anyway.

Signed-off-by: Stefano Fioravanzo <stefano@arrikto.com>
Reviewed-by: Ilias Katsakioris <elikatsis@arrikto.com>

kale/static_analysis/ast.py
@@ -125,9 +125,9 @@ def get_marshal_candidates(code):
     contexts = (ast.FunctionDef, ast.ClassDef, )
     tree = ast.parse(commented_code)
     for block in tree.body:
-        for node in walk(block):
+        for node in walk(block, stop_at=contexts):
             if isinstance(node, contexts):
-                break
+                names.add(node.name)
             if isinstance(node, (ast.Name,)):
                 names.add(node.id)
             if isinstance(node, (ast.Import, ast.ImportFrom,)):
@@ -141,27 +141,6 @@ def get_marshal_candidates(code):
     return names
 
 
-def get_function_args(fn):
-    """Get argument names of an ast.FunctionDef node.
-
-    Ref: https://greentreesnakes.readthedocs.io/en/latest/nodes.html#function-and-class-definitions  # noqa: E501
-
-    Args:
-        fn: ast.FunctionDef node
-
-    Returns (list(str)): list of function arguments
-    """
-    fn_args = []
-    for attr in ["args", "posonlyargs", "kwonlyargs"]:
-        for arg in getattr(fn.args, attr, []):
-            fn_args.append(arg.arg)
-    if fn.args.vararg:
-        fn_args.append(fn.args.vararg.arg)
-    if fn.args.kwarg:
-        fn_args.append(fn.args.kwarg.arg)
-    return fn_args
-
-
 def parse_functions(code):
     """Parse all the global functions present in the input code.
 
@@ -169,12 +148,11 @@ def parse_functions(code):
     source code. These also include function that are defined inside other
     Python statements, like `try`. ast.ClassDef nodes are skipped from the
     parsing so that class functions are ignored.
-    All the functions' arguments names are returned alongside the source code.
 
     Args:
         code (str): Multiline string representing Python code
 
-    Returns (dict): A dictionary [fn_name] -> (body, args)
+    Returns (dict): A dictionary [fn_name] -> function_source
     """
     fns = dict()
     tree = ast.parse(code)
@@ -184,10 +162,7 @@ def parse_functions(code):
                          ignore=(ast.ClassDef,)):
             if isinstance(node, (ast.FunctionDef,)):
                 fn_name = node.name
-                fn_args = get_function_args(node)
-                fn_body = "".join([astor.to_source(node)
-                                   for node in node.body])
-                fns[fn_name] = (fn_body, set(fn_args))
+                fns[fn_name] = astor.to_source(node)
     return fns
 
 

kale/static_analysis/dependencies.py
@@ -140,11 +140,11 @@ def detect_fns_free_variables(source_code, imports_and_functions="",
     """
     fns_free_vars = dict()
     # now check the functions' bodies for free variables. fns is a
-    # dict function_name -> (function_body, function_args)
+    # dict function_name -> function_source
     fns = kale_ast.parse_functions(source_code)
-    for fn_name, (fn_body, fn_args) in fns.items():
-        code = imports_and_functions + "\n" + fn_body
-        free_vars = pyflakes_report(code=code).difference(fn_args)
+    for fn_name, fn in fns.items():
+        code = imports_and_functions + "\n" + fn
+        free_vars = pyflakes_report(code=code)
         # the pipeline parameters that are used in the function
         consumed_params = {}
         if step_parameters:
@@ -202,12 +202,6 @@ def dependencies_detection(nb_graph: nx.DiGraph,
             pipeline_parameters=pipeline_parameters)
         fns_free_variables = detect_fns_free_variables(
             step_source_code, imports_and_functions, pipeline_parameters)
-        # will set ins later at the end of the function, as we will
-        # potentially add more ins below
-        nx.set_node_attributes(nb_graph, {step: {
-            'fns_free_variables': fns_free_variables,
-            'parameters': parameters
-        }})
 
         # Get all the function calls. This will be used below to check if any
         # of the ancestors declare any of these functions. Is that is so, the
@@ -237,30 +231,44 @@ def dependencies_detection(nb_graph: nx.DiGraph,
             # Include free variables
             to_remove = set()
             for fn_call in fn_calls:
-                fns_free_variables = anc_data.get("fns_free_variables", {})
-                if fn_call in fns_free_variables.keys():
+                anc_fns_free_vars = anc_data.get("fns_free_variables", {})
+                if fn_call in anc_fns_free_vars.keys():
                     # the current step needs to load these variables
-                    fn_free_vars, used_params = fns_free_variables[fn_call]
+                    fn_free_vars, used_params = anc_fns_free_vars[fn_call]
+                    # search if this function calls other functions (i.e. if
+                    # its free variables are found in the free variables dict)
+                    _left = list(fn_free_vars)
+                    while _left:
+                        _cur = _left.pop(0)
+                        # if the free var is itself a fn with free vars
+                        if _cur in anc_fns_free_vars:
+                            fn_free_vars.update(anc_fns_free_vars[_cur][0])
+                            _left = _left + list(anc_fns_free_vars[_cur][0])
                     ins.update(fn_free_vars)
                     # the current ancestor needs to save these variables
                     outs.update(fn_free_vars)
                     # add the parameters used by the function to the list
                     # of pipeline parameters used by the step
-                    step_params = step_data.get("parameters", {})
                     for param in used_params:
-                        if param not in step_params:
-                            step_params[param] = pipeline_parameters[param]
-                    nx.set_node_attributes(nb_graph,
-                                           {step: {'params': step_params}})
+                        parameters[param] = pipeline_parameters[param]
                     # Remove this function as it has been served. We don't want
                     # other ancestors to save free variables for this function.
                     # Using the helper to_remove because the set can not be
                     # resized during iteration.
                     to_remove.add(fn_call)
+                    # add the function and its free variables to the current
+                    # step as well. This is useful in case *another* function
+                    # will call this one (`fn_call`) in a child step. In this
+                    # way we can track the calls up to the last free variable.
+                    # (refer to test `test_dependencies_detection_recursive`)
+                    fns_free_variables[fn_call] = anc_fns_free_vars[fn_call]
             fn_calls.difference_update(to_remove)
             # Add to ancestor the new outs annotations. First merge the current
             # outs present in the anc with the new ones
             outs.update(anc_data.get('outs', []))
             nx.set_node_attributes(nb_graph, {anc: {'outs': sorted(outs)}})
 
-        nx.set_node_attributes(nb_graph, {step: {'ins': sorted(ins)}})
+        new_data = {'ins': sorted(ins),
+                    'fns_free_variables': fns_free_variables,
+                    'parameters': parameters}
+        nx.set_node_attributes(nb_graph, {step: new_data})

kale/tests/unit_tests/test_ast.py
@@ -66,8 +66,8 @@ def fun()
     ('', []),
     (_numpy_snippet, ['a', 'b', 'np', 'os', 'print']),
     (_numpy2_snippet, ['a', 'b', 'np', 'os', 'print']),
-    (_foos_snippet, []),
-    (_class_snippet, []),
+    (_foos_snippet, ['_test', '_test2']),
+    (_class_snippet, ['test']),
     (_ctx_mngr_snippet, ['my_context', 'param', 'res', 'ctx'])
 ])
 def test_get_marshal_candidates(code, target):
@@ -171,20 +171,7 @@ def foo():
     print(x)
     '''
 
-    target = {"foo": ("print('hello')\nprint(x)\n", set())}
-    assert kale_ast.parse_functions(code) == target
-
-
-def test_parse_functions_with_args():
-    """Test that the body of the function with args is retrieved correctly."""
-    code = '''
-x = 5
-def foo(a, b, c=None):
-    print('hello')
-    print(x)
-        '''
-
-    target = {"foo": ("print('hello')\nprint(x)\n", {"a", "b", "c"})}
+    target = {"foo": "def foo():\n    print('hello')\n    print(x)\n"}
     assert kale_ast.parse_functions(code) == target
 
 
@@ -202,15 +189,3 @@ def foo():
     print(x)
         '''
     assert kale_ast.get_function_calls(code) == {'print'}
-
-
-def test_function_args():
-    """Test the correct parsing of functions arguments."""
-    code = '''
-def f(a: 'annotation', b=1, c=2, *d, e, f=3, **g) -> 'return annotation':
-    pass
-    '''
-
-    tree = ast.parse(code)
-    args_names = ["a", "b", "c", "d", "e", "f", "g"]
-    assert sorted(kale_ast.get_function_args(tree.body[0])) == args_names

kale/tests/unit_tests/test_dependencies.py
@@ -91,7 +91,7 @@ foo()
     assert g.nodes(data=True)['step1']['ins'] == []
     assert g.nodes(data=True)['step1']['outs'] == ['x']
     assert g.nodes(data=True)['step2']['ins'] == ['x']
-    assert g.nodes(data=True)['step2']['outs'] == ['x']
+    assert g.nodes(data=True)['step2']['outs'] == ['foo', 'x']
     assert g.nodes(data=True)['step3']['ins'] == ['foo', 'x']
     assert g.nodes(data=True)['step3']['outs'] == []
 
@@ -128,7 +128,7 @@ print(x)
     assert g.nodes(data=True)['step1']['ins'] == []
     assert g.nodes(data=True)['step1']['outs'] == ['x']
     assert g.nodes(data=True)['step2']['ins'] == []
-    assert g.nodes(data=True)['step2']['outs'] == []
+    assert g.nodes(data=True)['step2']['outs'] == ['foo']
     assert g.nodes(data=True)['step3']['ins'] == ['foo', 'x']
     assert g.nodes(data=True)['step3']['outs'] == []
 
@@ -163,7 +163,7 @@ foo()
     assert g.nodes(data=True)['step1']['ins'] == []
     assert g.nodes(data=True)['step1']['outs'] == ['x']
     assert g.nodes(data=True)['step2']['ins'] == ['x']
-    assert g.nodes(data=True)['step2']['outs'] == ['x']
+    assert g.nodes(data=True)['step2']['outs'] == ['foo', 'x']
     assert g.nodes(data=True)['step3']['ins'] == ['foo', 'x']
     assert g.nodes(data=True)['step3']['outs'] == []
 
@@ -198,7 +198,7 @@ foo(5)
     assert g.nodes(data=True)['step1']['ins'] == []
     assert g.nodes(data=True)['step1']['outs'] == []
     assert g.nodes(data=True)['step2']['ins'] == []
-    assert g.nodes(data=True)['step2']['outs'] == []
+    assert g.nodes(data=True)['step2']['outs'] == ['foo']
     assert g.nodes(data=True)['step3']['ins'] == ['foo']
     assert g.nodes(data=True)['step3']['outs'] == []
 
@@ -234,7 +234,7 @@ foo(5)
     assert g.nodes(data=True)['step1']['ins'] == []
     assert g.nodes(data=True)['step1']['outs'] == []
     assert g.nodes(data=True)['step2']['ins'] == []
-    assert g.nodes(data=True)['step2']['outs'] == []
+    assert g.nodes(data=True)['step2']['outs'] == ['foo']
     assert g.nodes(data=True)['step3']['ins'] == ['foo']
     assert g.nodes(data=True)['step3']['outs'] == []
 
@@ -270,7 +270,7 @@ foo(5)
     assert g.nodes(data=True)['step1']['ins'] == []
     assert g.nodes(data=True)['step1']['outs'] == []
     assert g.nodes(data=True)['step2']['ins'] == []
-    assert g.nodes(data=True)['step2']['outs'] == []
+    assert g.nodes(data=True)['step2']['outs'] == ['foo']
     assert g.nodes(data=True)['step2']['parameters'] == {"y": (5, 'int')}
     assert g.nodes(data=True)['step3']['ins'] == ['foo']
     assert g.nodes(data=True)['step3']['outs'] == []
@@ -313,6 +313,177 @@ bar()
     assert g.nodes(data=True)['step1']['ins'] == []
     assert g.nodes(data=True)['step1']['outs'] == ['x', 'y']
     assert g.nodes(data=True)['step2']['ins'] == ['x', 'y']
-    assert g.nodes(data=True)['step2']['outs'] == ['x', 'y']
+    assert g.nodes(data=True)['step2']['outs'] == ['bar', 'foo', 'x', 'y']
     assert g.nodes(data=True)['step3']['ins'] == ['bar', 'foo', 'x', 'y']
     assert g.nodes(data=True)['step3']['outs'] == []
+
+
+def test_dependencies_detection_recursive():
+    """Test dependencies are detected even with a chain of functions calls."""
+    imports_and_functions = ""
+    pipeline_parameters = {}
+
+    g = nx.DiGraph()
+    # NODES
+    g.add_node("step1", ins=list(), outs=list(), source=['''
+%s
+x = 5
+    ''' % imports_and_functions])
+    g.add_node("step2", ins=list(), outs=list(), source=['''
+%s
+def foo():
+    print(x)
+def bar():
+    foo()
+    ''' % imports_and_functions])
+    g.add_node("step3", ins=list(), outs=list(), source=['''
+%s
+bar()
+    ''' % imports_and_functions])
+    # EDGES
+    g.add_edge("step1", "step2")
+    g.add_edge("step2", "step3")
+
+    dependencies.dependencies_detection(g, pipeline_parameters,
+                                        imports_and_functions)
+    assert g.nodes(data=True)['step1']['ins'] == []
+    assert g.nodes(data=True)['step1']['outs'] == ['x']
+    assert g.nodes(data=True)['step2']['ins'] == ['x']
+    assert g.nodes(data=True)['step2']['outs'] == ['bar', 'foo', 'x']
+    assert g.nodes(data=True)['step3']['ins'] == ['bar', 'foo', 'x']
+    assert g.nodes(data=True)['step3']['outs'] == []
+
+
+def test_dependencies_detection_recursive_different_steps():
+    """Test dependencies are detected even with a chain of functions calls."""
+    imports_and_functions = ""
+    pipeline_parameters = {}
+
+    g = nx.DiGraph()
+    # NODES
+    g.add_node("step1", ins=list(), outs=list(), source=['''
+%s
+x = 5
+def foo():
+    print(x)
+    ''' % imports_and_functions])
+    g.add_node("step2", ins=list(), outs=list(), source=['''
+%s
+def bar():
+    foo()
+    ''' % imports_and_functions])
+    g.add_node("step3", ins=list(), outs=list(), source=['''
+%s
+bar()
+    ''' % imports_and_functions])
+    # EDGES
+    g.add_edge("step1", "step2")
+    g.add_edge("step2", "step3")
+
+    dependencies.dependencies_detection(g, pipeline_parameters,
+                                        imports_and_functions)
+    assert g.nodes(data=True)['step1']['ins'] == []
+    assert g.nodes(data=True)['step1']['outs'] == ['foo', 'x']
+    assert g.nodes(data=True)['step2']['ins'] == ['foo', 'x']
+    assert g.nodes(data=True)['step2']['outs'] == ['bar', 'foo', 'x']
+    assert g.nodes(data=True)['step3']['ins'] == ['bar', 'foo', 'x']
+    assert g.nodes(data=True)['step3']['outs'] == []
+
+
+def test_dependencies_detection_recursive_different_steps_triple():
+    """Test dependencies are detected even with a long chain of fns calls."""
+    imports_and_functions = ""
+    pipeline_parameters = {}
+
+    g = nx.DiGraph()
+    # NODES
+    g.add_node("step0", ins=list(), outs=list(), source=['''
+    %s
+x = 5
+def init():
+    print(x)
+        ''' % imports_and_functions])
+    g.add_node("step1", ins=list(), outs=list(), source=['''
+%s
+def foo():
+    init()
+    ''' % imports_and_functions])
+    g.add_node("step2", ins=list(), outs=list(), source=['''
+%s
+def bar():
+    foo()
+    ''' % imports_and_functions])
+    g.add_node("step3", ins=list(), outs=list(), source=['''
+%s
+bar()
+    ''' % imports_and_functions])
+    # EDGES
+    g.add_edge("step0", "step1")
+    g.add_edge("step1", "step2")
+    g.add_edge("step2", "step3")
+
+    dependencies.dependencies_detection(g, pipeline_parameters,
+                                        imports_and_functions)
+    assert g.nodes(data=True)['step0']['ins'] == []
+    assert g.nodes(data=True)['step0']['outs'] == ['init', 'x']
+    assert g.nodes(data=True)['step1']['ins'] == ['init', 'x']
+    assert g.nodes(data=True)['step1']['outs'] == ['foo', 'init', 'x']
+    assert g.nodes(data=True)['step2']['ins'] == ['foo', 'init', 'x']
+    assert g.nodes(data=True)['step2']['outs'] == ['bar', 'foo', 'init', 'x']
+    assert g.nodes(data=True)['step3']['ins'] == ['bar', 'foo', 'init', 'x']
+    assert g.nodes(data=True)['step3']['outs'] == []
+
+
+def test_dependencies_detection_recursive_different_steps_branch():
+    """Test dependencies when fns are passed from multiple branches."""
+    imports_and_functions = ""
+    pipeline_parameters = {}
+
+    g = nx.DiGraph()
+    # NODES
+    g.add_node("step0", ins=list(), outs=list(), source=['''
+    %s
+x = 5
+y = 6
+        ''' % imports_and_functions])
+    g.add_node("stepL", ins=list(), outs=list(), source=['''
+%s
+def foo():
+    print(x)
+    ''' % imports_and_functions])
+    g.add_node("stepR", ins=list(), outs=list(), source=['''
+%s
+def bar():
+    print(y)
+    ''' % imports_and_functions])
+    g.add_node("stepM", ins=list(), outs=list(), source=['''
+%s
+def result():
+    foo()
+    bar()
+    ''' % imports_and_functions])
+    g.add_node("stepF", ins=list(), outs=list(), source=['''
+%s
+result()
+        ''' % imports_and_functions])
+    # EDGES
+    g.add_edge("step0", "stepL")
+    g.add_edge("step0", "stepR")
+    g.add_edge("stepL", "stepM")
+    g.add_edge("stepR", "stepM")
+    g.add_edge("stepM", "stepF")
+
+    dependencies.dependencies_detection(g, pipeline_parameters,
+                                        imports_and_functions)
+    assert g.nodes(data=True)['step0']['ins'] == []
+    assert g.nodes(data=True)['step0']['outs'] == ['x', 'y']
+    assert g.nodes(data=True)['stepL']['ins'] == ['x']
+    assert g.nodes(data=True)['stepL']['outs'] == ['foo', 'x']
+    assert g.nodes(data=True)['stepR']['ins'] == ['y']
+    assert g.nodes(data=True)['stepR']['outs'] == ['bar', 'y']
+    assert g.nodes(data=True)['stepM']['ins'] == ['bar', 'foo', 'x', 'y']
+    assert (g.nodes(data=True)['stepM']['outs']
+            == ['bar', 'foo', 'result', 'x', 'y'])
+    assert (g.nodes(data=True)['stepF']['ins']
+            == ['bar', 'foo', 'result', 'x', 'y'])
+    assert g.nodes(data=True)['stepF']['outs'] == []